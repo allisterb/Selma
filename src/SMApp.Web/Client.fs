52f47d5cc43ac988c58442d9cf76adca65aa0e57
Allister Beharry <allister.beharry@gmail.com>
2020-08-20 23:23:49 -0400

    Organize interpreters in Client.

--- src/SMApp.Web/Client.fs
+++ src/SMApp.Web/Client.fs
@@ -13,40 +13,36 @@ open SMApp.JQueryTerminal
 [<JavaScript>]
 module Client =
 
-    let (|Welcome|Help|Switch|Other|) =
-          function
-          | "help" -> Help
-          | "welcome" -> Welcome
-          | "switch" -> Switch
-          | _ -> Other
-
-    let i2 (this:Terminal) (command:string)   = 
-            match command with
-            | Help -> this.Echo("Commands: help, clear, template")
-            | Welcome -> 
-                async {
-                    this.Disable()
-                    match! Server.GetMeaning "Hello I'm John" with
-                    | None -> this.Echo "foo"
-                    | Some m -> this.Echo m.TopIntent.Name 
-                    this.Enable()
-                } |> Async.Start
-
-
-            | Switch -> this.Echo "switch"
-            | _ -> this.Echo "Unknown"
+    let Main (term:Terminal) (command:string)  = 
+        let (|Help|_|) =
+            function
+            | "help" -> Some "This is the help function"
+            | _ -> None 
+    
+        let (|Hello|_|) :Meaning -> Entity list option =
+            function
+            | m when m.TopIntent.Name = "Hello" && m.TopIntent.Confidence > 0.8f -> m.Entities |> Some
+            | _ -> None
+            
+        match command with
+        | Help h -> term.Echo h
+        | _ -> 
+            async {
+                do term.Disable()
+                match! Server.GetMeaning command with
+                | Some(Hello e) -> term.Echo "This is the hello intent"
+                | _ -> term.Echo "This is the whatever intent"
+                do term.Enable()
+            } |> Async.Start 
 
     let baseOpt =
         Options(
-            Name="_Base", 
+            Name="_", 
             Greetings = "Welcome to Selma",
-            OnInit = (fun (t:Terminal) -> t.Push(i2, Options(Name="Main", Prompt=">")))
+            OnInit = (fun (t:Terminal) -> t.Push(Main, Options(Name="Main", Prompt=">")))
         )
 
-    let baseInt =
-        ThisAction<Terminal, string>(fun this command->())
-
     let Term() = 
-        Terminal("#main", baseInt, baseOpt) |> ignore
+        Terminal("#main", ThisAction<Terminal, string>(fun _ _ ->()), baseOpt) |> ignore
         Doc.Empty
 
--- src/SMApp.Web/wwwroot/Scripts/WebSharper/SMApp.Web.js
+++ src/SMApp.Web/wwwroot/Scripts/WebSharper/SMApp.Web.js
@@ -95,69 +95,58 @@
  Client.Term=function()
  {
   var interpreter,options;
-  interpreter=Client.baseInt();
+  interpreter=Runtime.ThisFunc(function()
+  {
+   return null;
+  });
   options=Client.baseOpt();
   Global.$("#main").terminal(interpreter,options);
   return Doc.get_Empty();
- };
- Client.baseInt=function()
- {
-  SC$1.$cctor();
-  return SC$1.baseInt;
  };
  Client.baseOpt=function()
  {
   SC$1.$cctor();
   return SC$1.baseOpt;
  };
- Client.i2=function(_this,command)
+ Client.Main=function(term,command)
  {
   var a,b;
-  a=Client.Welcome(command);
-  a.$==1?_this.echo("Commands: help, clear, template"):a.$==0?Concurrency.Start((b=null,Concurrency.Delay(function()
+  function Hello(a$1)
   {
-   _this.disable();
-   return Concurrency.Combine(Concurrency.Bind((new AjaxRemotingProvider.New()).Async("SMApp.Web:SMApp.Web.Server.GetMeaning:477919497",["Hello I'm John"]),function(a$1)
+   return a$1.get_TopIntent().get_Name()==="Hello"&&a$1.get_TopIntent().get_Confidence()>0.8?{
+    $:1,
+    $0:a$1.get_Entities()
+   }:null;
+  }
+  a=command==="help"?{
+   $:1,
+   $0:"This is the help function"
+  }:null;
+  a!=null&&a.$==1?term.echo(a.$0):Concurrency.Start((b=null,Concurrency.Delay(function()
+  {
+   term.disable();
+   return Concurrency.Combine(Concurrency.Bind((new AjaxRemotingProvider.New()).Async("SMApp.Web:SMApp.Web.Server.GetMeaning:222565848",[command]),function(a$1)
    {
-    return a$1!=null&&a$1.$==1?(_this.echo(a$1.$0.get_TopIntent().get_Name()),Concurrency.Zero()):(_this.echo("foo"),Concurrency.Zero());
+    var $1,a$2;
+    return a$1!=null&&a$1.$==1&&(a$2=Hello(a$1.$0),a$2!=null&&a$2.$==1&&($1=a$2.$0,true))?(term.echo("This is the hello intent"),Concurrency.Zero()):(term.echo("This is the whatever intent"),Concurrency.Zero());
    }),Concurrency.Delay(function()
    {
-    _this.enable();
+    term.enable();
     return Concurrency.Zero();
    }));
-  })),null):a.$==2?_this.echo("switch"):_this.echo("Unknown");
- };
- Client.Welcome=function(a)
- {
-  return a==="help"?{
-   $:1,
-   $0:null
-  }:a==="welcome"?{
-   $:0,
-   $0:null
-  }:a==="switch"?{
-   $:2,
-   $0:null
-  }:{
-   $:3,
-   $0:null
-  };
+  })),null);
  };
  SC$1.$cctor=function()
  {
   var r;
   SC$1.$cctor=Global.ignore;
-  SC$1.baseOpt=(r={},r.name="_Base",r.greetings="Welcome to Selma",r.onInit=function(t)
+  SC$1.baseOpt=(r={},r.name="_",r.greetings="Welcome to Selma",r.onInit=function(t)
   {
    var r$1;
    return t.push(function(c)
    {
-    return Client.i2(this,c);
+    return Client.Main(this,c);
    },(r$1={},r$1.name="Main",r$1.prompt=">",r$1));
   },r);
-  SC$1.baseInt=Runtime.ThisFunc(function()
-  {
-   return null;
-  });
  };
 }());
